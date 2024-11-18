3dc4ca46f26613a09bb3854a01677155ebfc1dda
Cihan Calli <cihan.callii@gmail.com>
2024-11-18 20:33:35 +0300

    Navigation Arguments

--- app/src/main/java/com/zerdasoftware/foodbook/ListFragment.kt
+++ app/src/main/java/com/zerdasoftware/foodbook/ListFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.zerdasoftware.foodbook.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +20,25 @@ class ListFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.floatingActionButton.setOnClickListener { newAdd(it) }
+    }
+
+    fun newAdd(view: View){
+        val action = ListFragmentDirections.actionListFragmentToRecipeFragment(info = "new", id = 0)
+        Navigation.findNavController(view).navigate(action)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
--- app/src/main/java/com/zerdasoftware/foodbook/RecipeFragment.kt
+++ app/src/main/java/com/zerdasoftware/foodbook/RecipeFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.zerdasoftware.foodbook.databinding.FragmentRecipeBinding
 
 
 class RecipeFragment : Fragment() {
+
+    private var _binding: FragmentRecipeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +19,53 @@ class RecipeFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+        _binding = FragmentRecipeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.saveButton.setOnClickListener { save(it) }
+        binding.deleteButton.setOnClickListener { delete(it) }
+        binding.imageView.setOnClickListener { selectImage(it) }
+
+        arguments?.let {
+            val id = RecipeFragmentArgs.fromBundle(it).id
+            val info = RecipeFragmentArgs.fromBundle(it).info
+
+            if (info == "new"){
+                binding.deleteButton.visibility = View.GONE
+                binding.saveButton.visibility = View.VISIBLE
+
+                binding.foodNameEditText.setText("")
+                binding.foodIngredientsEditText.setText("")
+            } else {
+                binding.deleteButton.visibility = View.VISIBLE
+                binding.saveButton.visibility = View.GONE
+            }
+        }
+    }
+
+    fun save (view: View) {
+
+    }
+
+    fun delete (view: View) {
+
+    }
+
+
+
+    fun selectImage (view: View) {
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
